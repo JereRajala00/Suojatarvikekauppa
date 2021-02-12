676c8c0632f9ca791dfc2c4efae22530c246d4cf
n1417 <n1417@student.jamk.fi>
2021-02-12 14:21:38 +0200

    visualisointi

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import MainMenu from './MainMenu';
 import Header from './Header';
@@ -12,6 +11,12 @@ function App() {
       <MainMenu></MainMenu>
 
       <p>Tervetuloa suojatarvikekauppaan!<br></br>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.<br></br>
+      Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+      <br></br>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.<br></br>
+      Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+      <br></br>
       Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.<br></br>
       Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
       </p>
--- src/MainMenu.js
+++ src/MainMenu.js
@@ -1,8 +1,11 @@
+import React from 'react';
+
 const MainMenu = () => {
     const handleButton = () => {
         document.write("Testi");
     }
     return (<nav className="navbar">
+        <button onClick={handleButton}>Maskit</button>
         <button onClick={handleButton}>Maskit</button>
     </nav>  );
 }
