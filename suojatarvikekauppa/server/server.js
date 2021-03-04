70d113a5109759aff35d8786a6eba904769cd38e
Jere Rajala <jere.rajala80@gmail.com>
2021-03-04 16:20:30 +0200

    Add database connection string and install MySQL dependency

--- suojatarvikekauppa/server/server.js
+++ suojatarvikekauppa/server/server.js
@@ -12,3 +12,19 @@ const server = http.createServer((req, res) => {
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+  host: "mysql.labranet.jamk.fi",
+  user: "AA8660",
+  password: "JsozR5mq7k79pwuD5gW10nk8kGYetmmF",
+  database: "AA8660"
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  con.query("SELECT * FROM customers", function (err, result, fields) {
+    if (err) throw err;
+    console.log(result);
+  });
+});
