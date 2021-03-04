43b336efa0d1c80aa2331d21ed218d289c0f15ad
Jere Rajala <jere.rajala80@gmail.com>
2021-03-04 17:41:46 +0200

    Fix database communication

--- suojatarvikekauppa/server/server.js
+++ suojatarvikekauppa/server/server.js
@@ -1,18 +1,25 @@
+var mysql = require('mysql');
+var express = require('express');
+var app = express();
 const http = require('http');
 
 const hostname = '127.0.0.1';
 const port = 5000;
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('The server side of modern webstore');
-});
+var server = app.listen(5000, function () {
+    var host = server.address().address
+    var port = server.address().port
+    console.log("Example app listening at http://%s:%s", host, port)
+ })
+//const server = http.createServer((req, res) => {
+  //res.statusCode = 200;
+  //res.setHeader('Content-Type', 'text/plain');
+  //res.end('The server side of modern webstore');
+//});
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
-var mysql = require('mysql');
+//server.listen(port, hostname, () => {
+  //console.log(`Server running at http://${hostname}:${port}/`);
+//});
 
 var con = mysql.createConnection({
   host: "mysql.labranet.jamk.fi",
@@ -20,11 +27,13 @@ var con = mysql.createConnection({
   password: "JsozR5mq7k79pwuD5gW10nk8kGYetmmF",
   database: "AA8660"
 });
-
-con.connect(function(err) {
-  if (err) throw err;
-  con.query("SELECT * FROM customers", function (err, result, fields) {
-    if (err) throw err;
-    console.log(result);
-  });
-});
+app.get('/listUsers', function (req, res) {
+    con.connect(function(err) {
+        if (err) throw err;
+        con.query("SELECT * FROM customers", function (err, result, fields) {
+          if (err) throw err;
+          console.log(result);
+          res.send({ result })
+        });
+      });
+ })
