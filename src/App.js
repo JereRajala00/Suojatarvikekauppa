7e526ab6bf79ec591a119a9a7b7b10345e9496e9
n1417 <n1417@student.jamk.fi>
2021-03-23 16:23:46 +0200

    navbar siirretty header.js, etusivun galleria

--- src/App.js
+++ src/App.js
@@ -1,30 +1,58 @@
 import './App.css';
-import MainMenu from './MainMenu';
+import ReactDOM from 'react-dom';
 import Header from './Header';
-import Footer from './Footer';
+import Footer from './Footer.js';
+import React, { useEffect } from "react";
+import { useCycle } from "framer-motion";
+import {maskScene, desiScene, gloveScene} from "./scenes.js";
+import ImageHolder from "./ImageHolder";
+
+const SLIDE_CHANGE_TIME_MS = 5000;
 
 function App() {
+
+  const [currentScene, setCurrentScene] = useCycle(
+      maskScene,
+      desiScene,
+      gloveScene
+    );
+
+    useEffect(() => {
+      const timeOut = setTimeout(setCurrentScene, SLIDE_CHANGE_TIME_MS);
+      return () => clearTimeout(timeOut);
+    }, [currentScene, setCurrentScene]);
+
   return (
     <div className="App">
       <div className="content">
       <Header></Header>
-      <MainMenu></MainMenu>
+      
+      <div className="gallery">
+      <ImageHolder
+        img={currentScene.image}
+        className="gallery-image"
+      />
+      </div>
 
-      <p>Tervetuloa suojatarvikekauppaan!<br></br>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.<br></br>
-      Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+      <h3>Tervetuloa suojatarvikekauppaan!</h3>
+
+      <p>Täältä löydät kaiken tarvitsemasi suojatuaksesi COVID-19 -virukselta.<br></br>
+
+      Tuotteistamme löydät käsidesiä, kasvomaskeja jshaisfjksaf
       <br></br>
+
       Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.<br></br>
       Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
       <br></br>
       Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.<br></br>
       Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
       </p>
-
-      <Footer></Footer>
+  	    <Footer></Footer>
+      </div>
     </div>
-  </div>
+    
   );
 }
 
 export default App;
+
--- src/Header.js
+++ src/Header.js
@@ -1,12 +1,126 @@
+import MenuItems from './components/Navbar/MenuItems';
+import { createStore } from 'redux';
+import {AddItemToCart, DeleteItemFromCart, Counter} from './cart.js';
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom';
 
 
 const Header = () => {
+    let store = createStore(Counter);
+  //store.subscribe(() => console.log(store.getState()));
+  //store.dispatch(AddItemToCart())
+  const [selectedItem, setSelectedItem] = useState()
+  const [products, setProducts] = useState([])
+  const [isLoaded, setStateToLoaded] = useState(false)
     
     return (<div className="header">
-        <h1>Suojavarustekauppa</h1>
-        
+        <h1>Suojatarvikekauppa</h1>
 
+        <div className='navbar'>
+        {MenuItems.MenuItems.map((item, index) => (
+          <li key={index} onClick={() => setSelectedItem(item.title)}
+          style={{cursor: "pointer"}}>{item.title}</li>
+        ))}
+        {selectedItem && (
+          <h1>{selectedItem}</h1>
+        )}
+        {(selectedItem == 'Maskit') &&
+          <div><GetProductsHtml/></div>
+        }
+        {(selectedItem == 'Ostoskori') &&
+          <div><PlaceOrder/></div>
+        }
+        {(selectedItem == 'Rekisteröidy') &&
+          <div><RegisterAccount/></div>
+        }
+        {(selectedItem == 'Kirjaudu sisään') &&
+          <div><LoginForm/></div>
+        }
+        </div>
     </div> );
+
+async function initProducts() {
+    if (!isLoaded) {
+      fetch(`http://127.0.0.1:5000/listProducts`)
+      .then(response => response.json())
+      .then(response => {
+          setProducts(response.result);
+          console.log(response.result);
+          setStateToLoaded(true);
+  }
+      )
+    }
+    }
+
+function GetProductsHtml() {
+    initProducts()
+    const AddItem = () => {
+      console.log("Testi");
+      store.subscribe(() => console.log(store.getState()));
+      store.dispatch(AddItemToCart())
+    }
+    //store.dispatch(AddItemToCart())
+    //store.subscribe(() => console.log(store.getState()))
+    return products.map(products =>
+      <div key={products.ProductID}>
+        <h2>{products.ProductName}</h2>
+        <h2>{products.ProductDescription}</h2>
+        <h2>{products.ProductQuantity} units available</h2>
+        
+        <button onClick={AddItem}>Add to cart</button>
+        <button onClick={AddItem}>Show cart</button>
+        </div>
+    );
+}
+function PlaceOrder() {
+  return (
+  <div>
+  <form action="http://127.0.0.1:5000/placeOrder" method="post">
+  First name:<br/>
+  <input type="text" name="firstname"/><br/>
+  Last name:<br/>
+  <input type="text" name="lastname"/><br/>
+  Address:<br/>
+  <input type="text" name="address"/><br/>
+  <input type="submit" value="Submit"/>
+  </form>
+</div>
+  );
+}
+function RegisterAccount() {
+  return (
+    <div>
+    <form action="http://127.0.0.1:5000/registerAccount" method="post">
+    Etunimi:<br/>
+    <input type="text" name="firstname"/><br/>
+    Sukunimi:<br/>
+    <input type="text" name="lastname"/><br/>
+    Osoite:<br/>
+    <input type="text" name="address"/><br/>
+    Sähköposti:<br/>
+    <input type="text" name="email"/><br/>
+    Puhelin:<br/>
+    <input type="text" name="phone"/><br/>
+    Salasana:<br/>
+    <input type="password" name="password"/><br/>
+    <input type="submit" value="Submit"/>
+    </form>
+    </div>
+  );
+}
+function LoginForm() {
+  return (
+    <div>
+    <form action="http://127.0.0.1:5000/login" method="post">
+    Sähköposti:<br/>
+    <input type="text" name="email"/><br/>
+    Salasana:<br/>
+    <input type="password" name="password"/><br/>
+    <input type="submit" value="Submit"/>
+    </form>
+    </div>
+  );
+}
 }
  
 export default Header;
