8bbd3b3dfe9729c6adde8b2db92426722b3960c9
n1417 <n1417@student.jamk.fi>
2021-03-23 12:14:16 +0200

    visualisointi

--- src/Header.js
+++ src/Header.js
@@ -1,7 +1,17 @@
+import search from "./img/Search.png";
+import account from "./img/Account.png";
+import cart from "./img/Cart.png";
+
 const Header = () => {
     
     return (<div className="header">
         <h1>Suojavarustekauppa</h1>
+        <div className="header-menu">
+        <img src={search} alt="search"></img>
+        <img src={account} alt="account"></img>
+        <img src={cart} alt="cart"></img>
+        </div>
+
     </div> );
 }
  
